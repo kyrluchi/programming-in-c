8af76343b44faf190c19df9124214dc86c22e482
Kyr Luchi <kyr_lennon@yahoo.com.br>
2017-02-12 09:20:43 -0500

    Chapter 2 Initial Exercises

---
+++ chapter02/prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("Emanoel\n");
+
+  return 0;
+}
---
+++ chapter02/prog2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("programação é legal.\n");
+    printf("e programação em C é ainda mais legal\n");
+
+  return 0;
+}
