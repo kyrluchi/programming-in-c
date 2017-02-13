7bd72024a38de7bbf4a547f25b46887a33bb73f1
Kyr Luchi <kyr_lennon@yahoo.com.br>
2017-02-13 17:25:44 -0500

    Add exercise #6 source code

---
+++ chapter02/prog6.c
@@ -0,0 +1,22 @@
+/* Esse programa soma dois
+   inteiros e mostra o resultado */
+#include <stdio.h>
+
+int main (void)
+{
+   // Declare variables
+   int value1, value2;
+   int sum;
+
+   // Assign values and calculate their sum
+   value1 = 300;
+   value2 = 900;
+
+   sum = value1 + value2;
+
+   // Display the result
+   printf("A soma de %i + %i é igual a %i.\n", value2, value1, sum);
+  
+ return 0;
+}
+
