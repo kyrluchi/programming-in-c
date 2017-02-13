d56591b7256c9693c2ba437107035cb33d29c37b
Kyr Luchi <kyr_lennon@yahoo.com.br>
2017-02-13 17:24:29 -0500

    Add exercise #5 source code

---
+++ chapter02/prog5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int value1, value2, sum;
+
+    value1 = 10;
+    value2 = 15;
+
+    sum = value1 + value2;
+
+    printf("A soma de %i e %i é igual a %i.\n", value1, value2, sum);
+
+  return 0;
+}
+ 
