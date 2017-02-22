dfaf9f966512b561e7937442e4fe0bbac52d5360
Kyr Luchi <kyr_lennon@yahoo.com.br>
2017-02-21 19:09:27 -0500

    Add exercise #7 source code

---
+++ chapter02/prog7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("Em C, letras minusculas são diferentes de maíusculas\n");
+    printf ("main () é onde a execução do programa começa\n");
+    printf ("abrir e fechar chaves delimita linhas de uma função\ntodos os comandos precisam terminar com ponto e virgula.\n");
+
+  return 0;
+}
